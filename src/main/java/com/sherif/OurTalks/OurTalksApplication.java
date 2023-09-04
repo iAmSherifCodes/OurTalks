f0b1f519368e660c727aedf3624e452a482efb2a
Sherif Awofiranye <awofiranyesherif4@gmail.com>
2023-09-04 16:00:23 +0100

    Project set up

---
+++ src/main/java/com/sherif/OurTalks/OurTalksApplication.java
@@ -0,0 +1,13 @@
+package com.sherif.OurTalks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurTalksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OurTalksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sherif/OurTalks/OurTalksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sherif.OurTalks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurTalksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
